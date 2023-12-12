6a1f0cdced3cd407884d6e6bcdaa54cb4cd95490
sailesh kumar <skumar@petz.net>
2023-12-12 21:49:41 +0530

    update endpoints

--- vms/vms_app/urls.py
+++ vms/vms_app/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from vms_app.views import (VendorsAPIView, PurchaseOrderAPIView, AcknowledgePOAPIView, VendorsPerformanceAPIView)
 
 urlpatterns = [
-    path('vendors', VendorsAPIView.as_view(), name='vendors_list'),
-    path('vendors/<int:id>', VendorsAPIView.as_view(), name='vendor'),
+    path('vendors/', VendorsAPIView.as_view(), name='vendors_list'),
+    path('vendors/<int:id>/', VendorsAPIView.as_view(), name='vendor'),
     path('purchase_orders', PurchaseOrderAPIView.as_view(), name='purchase_orders'),
-    path('purchase_orders/<int:po_id>', PurchaseOrderAPIView.as_view(), name='purchase_order'),
-    path('purchase_orders/<int:po_id>/acknowledge', AcknowledgePOAPIView.as_view(), name='acknowledge'),
-    path('vendors/<int:id>/performance', VendorsPerformanceAPIView.as_view(), name='vendors_performance'),
+    path('purchase_orders/<int:po_id>/', PurchaseOrderAPIView.as_view(), name='purchase_order'),
+    path('purchase_orders/<int:po_id>/acknowledge/', AcknowledgePOAPIView.as_view(), name='acknowledge'),
+    path('vendors/<int:id>/performance/', VendorsPerformanceAPIView.as_view(), name='vendors_performance'),
 ]
