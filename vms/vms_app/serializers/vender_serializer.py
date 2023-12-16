1ff663c5d4bac21a324b69faf6b2d3c24ff5c6cd
sailesh kumar <skumar@petz.net>
2023-12-16 13:52:38 +0530

    validations

--- vms/vms_app/serializers/vender_serializer.py
+++ vms/vms_app/serializers/vender_serializer.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
+from django.core.validators import RegexValidator
 from vms_app.models import (Vendor, PurchaseOrder, HistoricalPerformance)
 
 class VendorSerializer(serializers.ModelSerializer):
+    
+    contact_details = serializers.CharField(
+        max_length=10,
+        validators=[
+            RegexValidator(
+                regex=r'^[6789]\d{9}$',
+                message='mobile numbers in India, which start with 6, 7, 8, or 9 and have 10 digits.'
+            )
+        ]
+    )
     class Meta:
         model=Vendor
         # fields = '__all__'
@@ -18,6 +29,21 @@ class PurchaseOrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = PurchaseOrder
         fields = '__all__'
+    
+    def validate(self, data):
+        if data.get('issue_date') and data.get('delivery_date'):
+            if data['delivery_date'] <= data['issue_date']:
+                raise serializers.ValidationError("Delivery date must be greater than the issue date.")
+            
+        if data.get('quality_rating') is not None and 0 > data.get('quality_rating') > 100:
+            raise serializers.ValidationError("Quality rating should be in between 1 to 100 or Null")
+        return data
+    
+    def validate_items(self, value):
+        for key, val in value.items():
+            if not isinstance(val, (int, float)):
+                raise serializers.ValidationError(f"The value Item '{key}' should be int or float.")
+        return value
     
     def to_representation(self, instance):
         representation = super().to_representation(instance)
